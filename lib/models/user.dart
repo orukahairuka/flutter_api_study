a0743d49b54d63200db548406aaa58894c1bb4e3
orukahairuka <sakuraierika03301109@gmail.com>
2024-01-25 12:58:43 +0900

    userクラス作成

---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+//Userモデルクラスの作成
+
+class User{
+  //コンストラクタ
+  User({
+    required this.id,
+    required this.profileImageUrl,
+  });
+
+
+  //プロパティ
+  final String id;
+  final String profileImageUrl;
+
+  //JSONからUserを生成するファクトリコンストラクタ
+factory User.fromJson(dynamic json){
+  return User(
+    id: json['id'] as String,
+    profileImageUrl: json['profile_image_url'] as String,
+  );
+}
+}
+
+
+
