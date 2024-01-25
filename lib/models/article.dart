2eb36e1d93118dbbc680dbde9983d8faf32e483c
orukahairuka <sakuraierika03301109@gmail.com>
2024-01-25 13:05:29 +0900

    feat articleモデルを作成

---
+++ lib/models/article.dart
@@ -0,0 +1,36 @@
+
+//article モデルクラスの作成
+import 'package:api_study/models/user.dart';
+
+class Article{
+  //コンストラクタ
+  Article({
+    required this.title,
+    required this.user,
+    this.likesCount = 0,
+    this.tags = const[],
+    required this.createdAt,
+    required this.url,
+  });
+
+  //プロパティ
+  final String title;
+  final User user;    
+  final int likesCount;
+  final List<String> tags;
+  final DateTime createdAt;
+  final String url;
+
+  //JSONからArticleを生成するファクトリコンストラクタ
+  factory Article.fromJson(dynamic json){
+    
+    return Article(
+      title: json['title'] as String,
+      user: User.fromJson(json['user']),
+      likesCount: json['likes_count'] as int,
+      tags: List<String>.from(json['tags'].map((tag) => tag['name'])),
+      createdAt: DateTime.parse(json['created_at'] as String),
+      url: json['url'] as String,
+    );
+  }
+}
